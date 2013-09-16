5f1466f870d1150dc49c004cb377a2a325c27de5
Basel <basel.alrudainy@gmail.com>
2013-09-16 11:31:06 +0200

    Update debug.ino

--- BlueCopter/debug.ino
+++ BlueCopter/debug.ino
@@ -1,13 +1,13 @@
 void debugProcess(){
-  #ifdef DEBUG_ANGLES
-   Serial.print("XT:");
+#ifdef DEBUG_ANGLES
+  Serial.print("XT:");
   Serial.print((float)(angles[0]));
   Serial.print('\t');
   Serial.print("YT:");
   Serial.print((float)(angles[1]));
   Serial.print('\t');
-  #endif
-  #ifdef DEBUG_GYRO
+#endif
+#ifdef DEBUG_GYRO
   Serial.print("GX:");
   Serial.print((float)(gx_aver));
   Serial.print('\t');
@@ -17,19 +17,16 @@ void debugProcess(){
   Serial.print("GZ:");
   Serial.print((float)(gz_aver));
   Serial.print('\t');
-  #endif
-  #ifdef DEBUG_PID
-  Serial.print("Kp:");
-  Serial.print((int)(kpr));
+#endif
+#ifdef DEBUG_PID
+  Serial.print("setX:");
+  Serial.print((float)(setX));
   Serial.print('\t');
-  Serial.print("Ki:");
-  Serial.print((int)(kpi));
+  Serial.print("setY:");
+  Serial.print((float)(setY));
   Serial.print('\t');
-  Serial.print("Kd:");
-  Serial.print((int)(kpd));
-  Serial.print('\t');
-  #endif
-  #ifdef DEBUG_RX
+#endif
+#ifdef DEBUG_RX
   Serial.print(F("Yaw:"));
   Serial.print(rxVal[0]);
   Serial.print('\t');
@@ -48,14 +45,14 @@ void debugProcess(){
   Serial.print(F("Throttle:"));
   Serial.print(rxVal[5]);
   Serial.print('\t');    
-  #endif
-  
-  #ifdef DEBUG_SERIAL_CHART
+#endif
+
+#ifdef DEBUG_SERIAL_CHART
   Serial.print(angles[0]);
   Serial.print(',');
   Serial.print(angles[1]);  
-  #endif
-  
+#endif
+
   Serial.print('\n');
-  
+
 }
