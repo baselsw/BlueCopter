c2b7533a738ec180a4aef1498d2e8ab53338bac3
Basel <basel.alrudainy@gmail.com>
2013-10-05 22:53:30 +0200

    Update BlueCopter.ino

--- BlueCopter/BlueCopter.ino
+++ BlueCopter/BlueCopter.ino
@@ -3,7 +3,7 @@
   
   Created by Basel Al-Rudainy, 6 april 2013.
   
-  This library is free software; you can redistribute it and/or
+  This firmware is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
